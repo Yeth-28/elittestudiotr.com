a245f087053069e673571159dafb76d6feaccbf1
Yeth-28 <talhameteacar1234@gmail.com>
2025-08-08 18:00:51 +0300

    .

---
+++ assets/js/carousel.js
@@ -0,0 +1,23 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+let currentSlideIndex = 0;
+
+function updateSlidePosition() {
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
+}
+
+nextButton.addEventListener('click', () => {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  updateSlidePosition();
+});
+
+prevButton.addEventListener('click', () => {
+  currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+  updateSlidePosition();
+});
+
+window.addEventListener('resize', updateSlidePosition);
